cf9be698892459b110fc2aaa630a71348a61d94a
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-12-27 00:45:54 -0500

    add avatar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,22 +9,22 @@ export default class Nav extends Component {
 
     render() {
         const { user } = this.props
+        const avatarSrc = "https://avatars3.githubusercontent.com/u/14981916?s=400&u=85620452a086a9dbaaad4a071ecfe0f85c985a86&v=4"
         return (
             <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light">
-                <h2><a className="navbar-brand" href="/">Chat App</a></h2>
+                <a className="navbar-brand" target="_blank" href="https://github.com/DiogenisPanagiotis"><img className="avatar" src={avatarSrc}/></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <Link to="/users"><span className="nav-link" href="/users">{user ? "Users" : null} <span className="sr-only">(current)</span></span></Link>
                         </li>
                         <li className="nav-item">
                             <Link to="/"><span className="nav-link" href="/">{user ? "Chat" : null}</span></Link>
                         </li>
                     </ul>
-                    <a target="_foo" href="https://github.com/DiogenisPanagiotis"><button className="btn btn-danger" type="submit">Github</button></a>
                 </div>
             </nav>
         );
