d7e9d81dd7ccfbf3ee356d6950acf7dd22e08057
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-12-28 15:07:24 -0500

    nav render optimization

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,8 +7,24 @@ export default class Nav extends Component {
         this.state = {}
     }
 
-    render() {
+    renderNavCollapse = () => {
         const { user } = this.props
+        return user ? 
+        (
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <Link to="/users"><span className="nav-link" href="/users">{user ? "Users" : null} <span className="sr-only">(current)</span></span></Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/"><span className="nav-link" href="/">{user ? "Chat" : null}</span></Link>
+                </li>
+            </ul>
+        )
+        :
+        <ul className="navbar-nav mr-auto"></ul>
+    }
+
+    render() {
         const avatarSrc = "https://avatars3.githubusercontent.com/u/14981916?s=400&u=85620452a086a9dbaaad4a071ecfe0f85c985a86&v=4"
         return (
             <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light">
@@ -17,14 +33,7 @@ export default class Nav extends Component {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link to="/users"><span className="nav-link" href="/users">{user ? "Users" : null} <span className="sr-only">(current)</span></span></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/"><span className="nav-link" href="/">{user ? "Chat" : null}</span></Link>
-                        </li>
-                    </ul>
+                {this.renderNavCollapse()}
                 </div>
             </nav>
         );
